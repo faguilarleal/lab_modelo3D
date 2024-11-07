a8d00a3a5ea1f9a6b402f361f22ffc7583e7cc46
faguilarleal <102198566+faguilarleal@users.noreply.github.com>
2024-11-06 23:47:59 -0600

    initial commit

---
+++ src/color.rs
@@ -0,0 +1,26 @@
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl Color {
+    pub fn new(red: u8, green: u8, blue: u8) -> Color {
+        Color { red, green, blue }
+    }
+
+    pub fn red(&self) -> u8 {
+        self.red
+    }
+
+    pub fn green(&self) -> u8 {
+        self.green
+    }
+
+    pub fn blue(&self) -> u8 {
+        self.blue
+    }
+
+    
+}
